b2cc3daa20df9559fb6f37821441022da9d77faf
rhenius7 <rheniusdeva92@gmail.com>
2023-11-07 11:43:13 +0530

    First git

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Message } from './Message';
+import { List } from './List';
+
+function App() {
+  return(
+        <div>
+          <Message></Message>
+          <List></List>
+        </div>
+  );
+}
+ 
+export default App;
---
+++ src/Message.jsx
@@ -0,0 +1,10 @@
+function Message()
+{
+    return (
+        <div>
+            <h1>From Message.jsx</h1>
+            <p>Hello World</p>
+        </div>
+    );
+}
+export {Message};
